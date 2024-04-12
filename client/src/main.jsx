290a0b816384851e8e402bfff764cde4e8fad579
UTandor <demonsouls34865@gmail.com>
2024-04-12 19:31:44 +0500

    Deployed successfully just trying this out

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from "@/components/ui/sonner"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Toaster />
+    <Analytics/>
+    <SpeedInsights/>
   </React.StrictMode>,
 )
